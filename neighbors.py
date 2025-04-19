b4b94ca3b57ad558d8b78bab7e44543c15cbc0b4
MyNatd <babymild@gmail.com>
2025-04-19 21:39:55 +0700

    Create neighbors.py

---
+++ neighbors.py
@@ -0,0 +1,14 @@
+# neighbors.py
+
+NEIGHBOR_AIRPORTS = {
+    "BKK": ["DMK", "CNX", "KBV", "HKT", "HDY", "USM"],
+    "DMK": ["BKK", "CNX", "HDY", "HKT", "KBV"],
+    "SIN": ["KUL", "PEN", "LGK"],
+    "KUL": ["SIN", "PEN", "LGK"],
+    "SGN": ["HAN", "DAD"],
+    "HKG": ["MFM", "ZUH"],
+    "TPE": ["KHH"],
+    "ICN": ["PUS"],
+    "RGN": ["MDL"],
+    "VTE": ["LPQ"],
+}
